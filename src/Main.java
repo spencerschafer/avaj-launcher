b18217fda8d95be1a0588b21b578372cad08eb04
spencerschafer <spencerschafer@gmail.com>
2018-06-12 14:21:52 +0200

    Update error handling in isFileValid() and add additional test files
    - Now checks for empty files, as well as files containing correct first
    line but doesn't include any aircraft.
    
    - Added empty file test and no aircraft test

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+import com.sun.org.apache.bcel.internal.classfile.LineNumber;
+
 import java.io.*;
-import java.security.SignedObject;
-import java.util.SortedMap;
 
 public class Main {
+	public static void main(String[] args) {
+		if (isFileValid(args[0]))
+			System.out.println("VALID FILE");
+	}
 
 	private static boolean isFirstLineValid(String[] line, int lineNumber) {
 
+		//checking that length is only 1 as we are expecting only 1 integer value on the first line
 		if (line.length != 1) {
 			System.out.println("ERROR: First line of file needs to be an integer.");
 			System.out.println("FORMAT: 25 (Example)");
@@ -13,6 +18,7 @@ public class Main {
 			return false;
 		}
 
+		//if the integer entered cannot be stored in an int, it is considered invalid
 		try {
 			Integer.parseInt(line[0]);
 		} catch (NumberFormatException e) {
@@ -45,6 +51,7 @@ public class Main {
 			return false;
 		}
 
+		//if the integer entered cannot be stored in an int, it is considered invalid
 		try {
 			Integer.parseInt(line[2]);
 			Integer.parseInt(line[3]);
@@ -56,16 +63,22 @@ public class Main {
 			return false;
 		}
 		return true;
-
 	}
 
 	private static boolean isFileValid(String file) {
 		boolean valid;
+		int lineNumber = 1;
+		File input = new File(file);
 
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+		//checking for empty file
+		if (input.length() == 0) {
+			System.out.println("ERROR: Empty file.");
+			return false;
+		}
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(input)))) {
 			String line;
 			String[] string;
-			int lineNumber = 0;
 
 			//getting first line
 			if ((line = br.readLine()) != null) {
@@ -76,7 +89,7 @@ public class Main {
 				lineNumber++;
 			}
 
-			//iterating through subsequent lines
+			//iterating through subsequent file lines
 			while ((line = br.readLine()) != null) {
 				string = line.split(" ");
 				valid = isLineValid(string, lineNumber);
@@ -84,15 +97,30 @@ public class Main {
 					return false;
 				lineNumber++;
 			}
+
 		} catch (IOException e) {
-			System.out.println("ERROR: readFile\nCLASS: Main.java\n");
+			System.out.println("ERROR: IOException in function isFileValid() in class Main.java]\n");
+			return false;
+		}
+
+		//if correct integer was entered on first line, but no following lines were entered
+		if (lineNumber == 2) {
+			System.out.println("ERROR: No aircrafts were entered.");
+			System.out.println("FORMAT: [TYPE] [NAME] [LONGITUDE] [LATITUDE] [HEIGHT]");
+			System.out.println("See line [" + lineNumber + "]. ");
 			return false;
 		}
 		return true;
 	}
+/*
+	public static void createAircraft(String[] line) {
+		Aircraft aircraft;
+		String name = line[1];
+		int longitude = Integer.parseInt(line[2]);
+		int latitude = Integer.parseInt(line[3];
+		int height = Integer.parseInt(line[4]);
 
-	public static void main(String[] args) {
-		if (isFileValid(args[0]))
-			System.out.println("VALID FILE");
-	}
+
+
+	}*/
 }
